bd2e572f4fac244d3766274d87c0aef046c869ae
Jules LEROQUAIS <leroquais.jules@gmail.com>
2024-10-03 18:41:19 +0200

    init sha3.c file

--- src/sha3.c
+++ src/sha3.c
@@ -1,6 +1,15 @@
 #include "sha3.h"
 
-int main(void){
-	printf("Hello world\n");
+int main(int argc, char **argv){
+	printf("Hello from the sha3 world!\n");
+	if(argc < 2){
+		printf("Sha3 list:\n");
+		printf("  * sha3-224\n");
+		printf("  * sha3-256\n");
+		printf("  * sha3-384\n");
+		printf("  * sha3-512\n");
+		return 1;
+	}
+	printf("%s\n", argv[1]);
 	return 0;
 }
