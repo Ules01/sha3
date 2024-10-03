747da8ab8723bce76d043f8a1628ddf9f82e0d20
Jules LEROQUAIS <leroquais.jules@gmail.com>
2024-10-03 19:02:36 +0200

    init struct

--- src/sha3.c
+++ src/sha3.c
@@ -1,8 +1,30 @@
 #include "sha3.h"
 
 int main(int argc, char **argv){
-	printf("Hello from the sha3 world!\n");
-	if(argc < 2){
+	sha3 *sha3 = NULL;
+	int i = 1;
+
+	//Check args
+	while(i < argc){
+		//Select sha3
+		if(!strcmp(argv[i], "sha3-224")){
+			sha3 = &SHA3_224;
+		}
+		if(!strcmp(argv[i], "sha3-256")){
+			sha3 = &SHA3_256;
+		}
+		if(!strcmp(argv[i], "sha3-384")){
+			sha3 = &SHA3_384;
+		}
+		if(!strcmp(argv[i], "sha3-512")){
+			sha3 = &SHA3_512;
+		}
+
+		i++;
+	}
+
+	//Check if a sha3 has been selected
+	if(!sha3){
 		printf("Sha3 list:\n");
 		printf("  * sha3-224\n");
 		printf("  * sha3-256\n");
@@ -10,6 +32,9 @@ int main(int argc, char **argv){
 		printf("  * sha3-512\n");
 		return 1;
 	}
-	printf("%s\n", argv[1]);
+
+	//Start message
+	printf("Hello from the sha3 world!\n");
+
 	return 0;
 }
